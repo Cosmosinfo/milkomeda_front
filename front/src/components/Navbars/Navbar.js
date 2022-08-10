34cf246a08c441bf51862e6852b76e6bd7a33bf5
muse9312 <muse9312@naver.com>
2022-08-10 16:48:11 +0900

    Navbar modify

--- front/src/components/Navbars/Navbar.js
+++ front/src/components/Navbars/Navbar.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import '../../assets/css/Navbar.css'
+import home from '../../assets/icon/ping/home.svg'
 
 function Navbar() {
     return (
         <div className="nav">
+            <div>
+                <p className="logo_text">milkomeda</p>
+            </div>
 
-            <p className="logo_text">milkomeda</p>
+            <div className="home_btn">
+                <img src={home} alt="home" />
+                <p className="home_text">Home</p>
+            </div>
+
 
 
         </div>
