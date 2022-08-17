53ec79b6736ed0027a08da1830ae946089d7bf96
muse9312 <muse9312@naver.com>
2022-08-17 13:47:52 +0900

    Create  Carousel Components

---
+++ front/src/components/Carousel/Carousel.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../../assets/css/Carousel.css"
+
+function Carousel() {
+    return (
+        <div className="Carousel">
+
+            Carousel
+        </div>
+    )
+}
+
+export default Carousel
--- front/src/pages/home/Home.js
+++ front/src/pages/home/Home.js
@@ -2,18 +2,19 @@ import Chart from "../../components/chart/Chart";
 import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo";
 import "../../assets/css/home.css";
 import { userData } from "../../dummyData";
+import Carousel from "../../components/Carousel/Carousel";
 import WidgetSm from "../../components/widgetSm/WidgetSm";
 import WidgetLg from "../../components/widgetLg/WidgetLg";
 
 export default function Home() {
   return (
     <div className="home">
-      <WidgetLg />
+
       {/* <FeaturedInfo />
-      <Chart data={userData} title="User Analytics" grid dataKey="Active User" />
-      <div className="homeWidgets">
-        <WidgetSm />
-        <WidgetLg /> */}
+      <Chart data={userData} title="User Analytics" grid dataKey="Active User" /> */}
+      <div className="HomeCarousel">
+        <Carousel />
+      </div>
     </div>
 
   );
