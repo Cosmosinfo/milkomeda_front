2f81031bdf00afee29d3801645c0514ae855655d
muse9312 <muse9312@naver.com>
2022-08-17 16:02:34 +0900

    MainPage layout

---
+++ front/src/components/MainPage/NewArtist/NewArtist.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import { ArrowDownward, ArrowUpward } from "@material-ui/icons";
+import "../../../assets/css/Main/NewArtist.css"
+
+function NewArtist() {
+    return (
+        <div className="Artist">
+            <div className="featuredItem">
+                <div className="featuredMoneyContainer">
+                    <img src="https://images.pexels.com/photos/1526814/pexels-photo-1526814.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" className="ArtistImg" />
+                    <div className="ArtistUser">
+                        <h6 className="ArtistName">Artist Name</h6>
+                        <span className="MusicGenre">Music Genre</span>
+                    </div>
+                </div>
+            </div>
+            <div className="featuredItem">
+                <div className="featuredMoneyContainer">
+                    <img src="https://images.pexels.com/photos/1526814/pexels-photo-1526814.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" className="ArtistImg" />
+                    <div className="ArtistUser">
+                        <h6 className="ArtistName">Artist Name</h6>
+                        <span className="MusicGenre">Music Genre</span>
+                    </div>
+                </div>
+            </div>
+            <div className="featuredItem">
+                <div className="featuredMoneyContainer">
+                    <img src="https://images.pexels.com/photos/1526814/pexels-photo-1526814.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" className="ArtistImg" />
+                    <div className="ArtistUser">
+                        <h6 className="ArtistName">Artist Name</h6>
+                        <span className="MusicGenre">Music Genre</span>
+                    </div>
+                </div>
+            </div>
+            <div className="featuredItem">
+                <div className="featuredMoneyContainer">
+                    <img src="https://images.pexels.com/photos/1526814/pexels-photo-1526814.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" className="ArtistImg" />
+                    <div className="ArtistUser">
+                        <h6 className="ArtistName">Artist Name</h6>
+                        <span className="MusicGenre">Music Genre</span>
+                    </div>
+                </div>
+            </div>
+            <div className="featuredItem">
+                <div className="featuredMoneyContainer">
+                    <img src="https://images.pexels.com/photos/1526814/pexels-photo-1526814.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" className="ArtistImg" />
+                    <div className="ArtistUser">
+                        <h6 className="ArtistName">Artist Name</h6>
+                        <span className="MusicGenre">Music Genre</span>
+                    </div>
+                </div>
+            </div>
+            <div className="featuredItem">
+                <div className="featuredMoneyContainer">
+                    <img src="https://images.pexels.com/photos/1526814/pexels-photo-1526814.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" className="ArtistImg" />
+                    <div className="ArtistUser">
+                        <h6 className="ArtistName">Artist Name</h6>
+                        <span className="MusicGenre">Music Genre</span>
+                    </div>
+                </div>
+            </div>
+
+
+
+        </div>
+    )
+}
+
+export default NewArtist
