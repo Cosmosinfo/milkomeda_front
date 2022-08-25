60c5b8ecbccea219c261a83bc7f67c63a02ca5b5
muse9312 <muse9312@naver.com>
2022-08-25 23:07:38 +0900

    code refactoring

--- front/src/pages/Error/ErrorPage.js
+++ front/src/pages/Error/ErrorPage.js
@@ -5,40 +5,40 @@ function ErrorPage() {
   return (
     <>
 
-        <div className='ErrorPage'>
-            <div className='ErrorPage_Wrapper'>
-                <div className='ErrorPage_left'>
-                  
+      <div className='ErrorPage'>
+        <div className='ErrorPage_Wrapper'>
+          <div className='ErrorPage_left'>
+
+          </div>
+
+          <div className='ErrorPage_right'>
+            <div className='ErrorPage_right_ErrorBox'>
+              <div className='ErrorPage_right_ErrorBox_top'>
+                Page not found
+              </div>
+
+              <div className='ErrorPage_right_ErrorBox_middle'>
+                <span className='ErrorPage_right_ErrorBox_middle_big'>Oops! Error 404</span>
+                <span className='ErrorPage_right_ErrorBox_middle_small'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris</span>
+              </div>
+
+              <div className='ErrorPage_right_ErrorBox_Btm'>
+                <div className='ErrorPage_right_ErrorBox_Btm_text1'>
+                  Back to Homepage
                 </div>
 
-                <div className='ErrorPage_right'>
-                    <div className='ErrorPage_right_ErrorBox'>
-                      <div className='ErrorPage_right_ErrorBox_top'>
-                        Page not found
-                      </div>
-
-                      <div className='ErrorPage_right_ErrorBox_middle'>
-                          <span className='ErrorPage_right_ErrorBox_middle_big'>Oops! Error 404</span>
-                          <span className='ErrorPage_right_ErrorBox_middle_small'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris</span>
-                      </div>
-
-                      <div className='ErrorPage_right_ErrorBox_Btm'>
-                        <div className='ErrorPage_right_ErrorBox_Btm_text1'>
-                          Back to Homepage
-                        </div>
-
-                        <div className='ErrorPage_right_ErrorBox_Btm_text2'>
-                          Visit our help center
-                        </div>
-                      </div>
-                    </div>
+                <div className='ErrorPage_right_ErrorBox_Btm_text2'>
+                  Visit our help center
                 </div>
+              </div>
             </div>
-
+          </div>
         </div>
 
+      </div>
 
-    
+
+
     </>
   )
 }
