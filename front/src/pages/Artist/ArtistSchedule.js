dd9441cd2c6e2a37d40588fdc9e0c07719e30386
muse9312 <muse9312@naver.com>
2022-10-24 17:28:28 +0900

    test

--- front/src/pages/Artist/ArtistSchedule.js
+++ front/src/pages/Artist/ArtistSchedule.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Link } from "react-router-dom";
 import '../../assets/css/Artist/ArtistSchedule.css'
 import ArtistInfoCard from '../../components/ArtistInfoCard/ArtistInfoCard';
