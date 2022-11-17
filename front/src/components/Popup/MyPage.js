95fb4fb2f658f495324b957460d9d9d43e51acec
muse9312 <muse9312@naver.com>
2022-11-17 18:47:06 +0900

    fix : store jp lang modify

--- front/src/components/Popup/MyPage.js
+++ front/src/components/Popup/MyPage.js
@@ -4,9 +4,14 @@ import './MyPage.css'
 import edit from '../../assets/icon/graw/edit-2.svg'
 import myshop from '../../assets/icon/graw/shopping-cart.svg'
 import setting from '../../assets/icon/graw/settings.svg'
+import { useTranslation } from "react-i18next";
 
 
 function MyPage(open) {
+
+    const { t } = useTranslation();
+
+
     if (!open) {
         return null
     }
@@ -21,7 +26,7 @@ function MyPage(open) {
 
                         <div className='popHeader-textbox'>
                             <div className='popHeader-textbox-un'>
-                                User Name
+                                {t("popup_mypage1")}
                             </div>
 
                             <div className='popHeader-textbox-sb'>
@@ -41,19 +46,19 @@ function MyPage(open) {
                         <div className='popfooter-Content'>
                             <div className="popfooter-ContentItem">
                                 <img className="popfooter-ContentEditIcon" src={edit} alt="edit" />
-                                <span className="popfooter-ContentItem-text">Edit Profile</span>
+                                <span className="popfooter-ContentItem-text">{t("popup_mypage2")}</span>
                             </div>
 
                             <div className="popfooter-ContentItem">
                                 <img className="popfooter-ContentEditIcon" src={myshop} alt="myshop" />
-                                <span className="popfooter-ContentItem-text">My Shopping</span>
-                                
+                                <span className="popfooter-ContentItem-text">{t("popup_mypage3")}</span>
+
                             </div>
 
                             <div className="popfooter-ContentItem">
                                 <img className="popfooter-ContentEditIcon" src={setting} alt="setting" />
-                                <span className="popfooter-ContentItem-text">Setting</span>
-                                
+                                <span className="popfooter-ContentItem-text">{t("popup_mypage4")}</span>
+
                             </div>
                         </div>
                     </div>
