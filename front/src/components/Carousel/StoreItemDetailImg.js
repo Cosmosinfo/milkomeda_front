162e13ab129a20bbc007df4fef517286767afde9
tkddus614 <tkddus614@naver.com>
2022-11-24 17:53:48 +0900

    MIL-7 refactor:Carousel

--- front/src/components/Carousel/StoreItemDetailImg.js
+++ front/src/components/Carousel/StoreItemDetailImg.js
@@ -1,23 +1,19 @@
-import React from 'react'
-
+import React from "react";
 import { Carousel } from "./Carousel";
 
 function StoreItemDetailImg() {
-    const images = [
-        {
-        
-          image: "./img/img9.jpg",
-         
-        }
-      ];
-    return (
-        <>
-
-        <Carousel
+  const images = [
+    {
+      image: "./img/img9.jpg",
+    },
+  ];
+  return (
+    <>
+      <Carousel
         //accepts news data
         data={images}
         //time interval milliseconds between each picture
-        
+
         //width of carousel
         width="794px"
         //height of carousel
@@ -25,14 +21,13 @@ function StoreItemDetailImg() {
         //constitutes the border radius of each slide px
         radius="6px"
         //enables automatic transitioning between slides
-        
+
         //enables visual dots between slides
-        
+
         //changes object fit I used cover to avoud stretching
-        
       />
-       </>
-    )
+    </>
+  );
 }
 
-export default StoreItemDetailImg
+export default StoreItemDetailImg;
--- front/src/components/Carousel/StoreItemImg.js
+++ front/src/components/Carousel/StoreItemImg.js
@@ -1,23 +1,20 @@
-import React from 'react'
+import React from "react";
 
 import { Carousel } from "./Carousel";
 
 function StoreItemImg() {
-    const images = [
-        {
-        
-          image: "./img/img9.jpg",
-         
-        }
-      ];
-    return (
-        <>
-
-        <Carousel
+  const images = [
+    {
+      image: "./img/img9.jpg",
+    },
+  ];
+  return (
+    <>
+      <Carousel
         //accepts news data
         data={images}
         //time interval milliseconds between each picture
-        
+
         //width of carousel
         width="250px"
         //height of carousel
@@ -25,14 +22,13 @@ function StoreItemImg() {
         //constitutes the border radius of each slide px
         radius="6px"
         //enables automatic transitioning between slides
-        
+
         //enables visual dots between slides
-        
+
         //changes object fit I used cover to avoud stretching
-        
       />
-       </>
-    )
+    </>
+  );
 }
 
-export default StoreItemImg
+export default StoreItemImg;
