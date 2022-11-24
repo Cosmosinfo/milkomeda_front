53fe72a300b4ebfd6808243166856b4bbd94c114
tkddus614 <tkddus614@naver.com>
2022-11-24 18:12:36 +0900

    MIL-7 refactor:live message

--- front/src/components/LiveMessageBox/LiveMessageBox.js
+++ front/src/components/LiveMessageBox/LiveMessageBox.js
@@ -1,39 +1,97 @@
-import React from 'react'
-import './LiveMessageBox.css'
+import React from "react";
+import styled from "styled-components";
 import { useTranslation } from "react-i18next";
 
 function LiveMessageBox() {
-
-  // 다국어
   const { t } = useTranslation();
 
   return (
     <>
-
-      <div className='StreamLive_Right_btm'>
-        <div className='StreamLive_ChatBox'>
-          <div className='StreamLive_ChatBox_UserName'>
-            <div className="StreamLive_ChatBox_UserName_img">
-
-              <img src="https://images.pexels.com/photos/1526814/pexels-photo-1526814.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" className="StreamLive_UserName_img" />
-
-            </div>
-            <div className="StreamLive_ChatBox_UserName_text">{t("streamcb_usermain")}</div>
-            <div className="StreamLive_ChatBox_time_text">17:00</div>
-          </div>
-
-          <div className='StreamLive_ChatBox_chatContainer'>
-            <div className='StreamLive_ChatBox_chatContainer_text'>Lorem ipsum dolor sit amet,
-              consectetur adipiscing elit
-
-            </div>
-          </div>
-
-        </div>
-      </div>
-
+      <StreamLiveRightbtm>
+        <StreamLiveChatBox>
+          <StreamLiveChatBoxUserName>
+            <StreamLiveChatBoxUserNameImg>
+              <img src="https://images.pexels.com/photos/1526814/pexels-photo-1526814.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" />
+            </StreamLiveChatBoxUserNameImg>
+            <StreamLiveChatBoxUserNameText>{t("streamcb_usermain")}</StreamLiveChatBoxUserNameText>
+            <StreamLiveChatBoxtimeText>17:00</StreamLiveChatBoxtimeText>
+          </StreamLiveChatBoxUserName>
+          <StreamLiveChatBoxChatContainer>
+            <StreamLiveChatBoxChatContainerText>Lorem ipsum dolor sit amet, consectetur adipiscing elit</StreamLiveChatBoxChatContainerText>
+          </StreamLiveChatBoxChatContainer>
+        </StreamLiveChatBox>
+      </StreamLiveRightbtm>
     </>
-  )
+  );
 }
 
-export default LiveMessageBox
+const StreamLiveRightbtm = styled.div`
+  /* width: 308px;
+    height: 108px; */
+  padding: 12px 0;
+`;
+
+const StreamLiveChatBox = styled.div`
+  /* width: 308px; */
+  height: 100%;
+`;
+
+const StreamLiveChatBoxUserName = styled.div`
+  /* width: 100%; */
+  height: 30px;
+  display: flex;
+`;
+
+const StreamLiveChatBoxUserNameImg = styled.div`
+  width: 30px;
+  height: 30px;
+  margin-right: 12px;
+  img {
+    width: 30px;
+    height: 30px;
+    border-radius: 100px;
+    cursor: pointer;
+  }
+`;
+
+const StreamLiveChatBoxUserNameText = styled.div`
+  width: 100%;
+  height: 100%;
+  font-weight: 400;
+  text-align: left;
+  font-size: 14px;
+  line-height: 30px;
+  /* identical to box height */
+  color: #ffffff;
+`;
+
+const StreamLiveChatBoxtimeText = styled.div`
+  /* width : 100%; */
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 30px;
+  /* identical to box height */
+  text-align: right;
+  color: #8b8999;
+`;
+
+const StreamLiveChatBoxChatContainer = styled.div`
+  /* width: 100%; */
+  height: 70px;
+  margin-top: 12px;
+  background: #15141b;
+  border-radius: 6px;
+`;
+
+const StreamLiveChatBoxChatContainerText = styled.div`
+  box-sizing: border-box;
+  padding: 12px 24px;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 21px;
+  text-align: left;
+  color: #ffffff;
+`;
+
+export default LiveMessageBox;
