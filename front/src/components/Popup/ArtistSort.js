d18f502825e15df6478c2ff7d69c95c519b3b683
tkddus614 <tkddus614@naver.com>
2022-11-25 11:11:01 +0900

    MIL-7 refactor : Popup

--- front/src/components/Popup/ArtistSort.js
+++ front/src/components/Popup/ArtistSort.js
@@ -1,61 +1,68 @@
-import React from 'react'
-import './ArtistSort.css'
+import React from "react";
+import styled from "styled-components";
 import { useTranslation } from "react-i18next";
 
 function ArtistSort() {
+  const { t } = useTranslation();
 
-    const { t } = useTranslation();
-
-    return (
-        <>
-            <div className='ArtistSort-popWrapper'>
-                <div className='ArtistSort-contentItem'>
-                    <div className='ArtistSort-contentItemText'>
-                        {t("artistsort_elec")}
-                    </div>
-
-                </div>
-
-                <div className='ArtistSort-contentItem'>
-                    <div className='ArtistSort-contentItemText'>
-                        {t("artistsort_hip")}
-                    </div>
-
-                </div>
-
-                <div className='ArtistSort-contentItem'>
-                    <div className='ArtistSort-contentItemText'>
-                        {t("artistsort_metal")}
-                    </div>
-
-                </div>
-
-                <div className='ArtistSort-contentItem'>
-                    <div className='ArtistSort-contentItemText'>
-                        {t("artistsort_rb")}
-                    </div>
-
-                </div>
-
-                <div className='ArtistSort-contentItem'>
-                    <div className='ArtistSort-contentItemText'>
-                        {t("artistsort_jazz")}
-                    </div>
-
-                </div>
-
-                <div className='ArtistSort-contentItem'>
-                    <div className='ArtistSort-contentItemText'>
-                        {t("artistsort_pop")}
-                    </div>
-
-                </div>
-
-
-            </div>
-
-        </>
-    )
+  return (
+    <>
+      <ArtistSortPopWrapper>
+        <ArtistSortContentItem>
+          <ArtistSortContentItemText>{t("artistsort_elec")}</ArtistSortContentItemText>
+        </ArtistSortContentItem>
+        <ArtistSortContentItem>
+          <ArtistSortContentItemText>{t("artistsort_hip")}</ArtistSortContentItemText>
+        </ArtistSortContentItem>
+        <ArtistSortContentItem>
+          <ArtistSortContentItemText>{t("artistsort_metal")}</ArtistSortContentItemText>
+        </ArtistSortContentItem>
+        <ArtistSortContentItem>
+          <ArtistSortContentItemText>{t("artistsort_rb")}</ArtistSortContentItemText>
+        </ArtistSortContentItem>
+        <ArtistSortContentItem>
+          <ArtistSortContentItemText>{t("artistsort_jazz")}</ArtistSortContentItemText>
+        </ArtistSortContentItem>
+        <ArtistSortContentItem>
+          <ArtistSortContentItemText>{t("artistsort_pop")}</ArtistSortContentItemText>
+        </ArtistSortContentItem>
+      </ArtistSortPopWrapper>
+    </>
+  );
 }
 
-export default ArtistSort
+const ArtistSortPopWrapper = styled.div`
+  /* width: 100px; */
+  height: auto;
+  background: #191922;
+  border-radius: 10px;
+  box-sizing: border-box;
+`;
+
+const ArtistSortContentItem = styled.div`
+  /* width: 100%; */
+  height: 40px;
+  border-top: 1px solid rgba(139, 137, 153, 0.2);
+  color: #ffffff;
+  display: flex;
+  justify-content: center;
+  &:first-of-type {
+    border: none;
+    display: flex;
+    justify-content: center;
+  }
+`;
+
+const ArtistSortContentItemText = styled.div`
+  /* width: 100%; */
+  height: 17px;
+  padding: 12px 12px;
+  cursor: pointer;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 17px;
+  /* identical to box height */
+`;
+
+export default ArtistSort;
--- front/src/components/Popup/StreamSort.js
+++ front/src/components/Popup/StreamSort.js
@@ -1,35 +1,56 @@
-import React from 'react'
-import './StreamSort.css'
+import React from "react";
+import styled from "styled-components";
 import { useTranslation } from "react-i18next";
 
-
 function StreamSort() {
+  const { t } = useTranslation();
 
-    // 다국어
-    const { t } = useTranslation();
-
-    return (
-        <>
-            <div className='StreamSort-popWrapper'>
-                <div className='StreamSort-contentItem'>
-                    <div className='StreamSort-contentItemText'>
-                        {t("streamsort_user")}
-                    </div>
-
-                </div>
-
-                <div className='StreamSort-contentItem'>
-                    <div className='StreamSort-contentItemText'>
-                        {t("streamsort_artist")}
-                    </div>
-
-                </div>
-
-
-            </div>
-
-        </>
-    )
+  return (
+    <>
+      <StreamSortPopWrapper>
+        <StreamSortContentItem>
+          <StreamSortContentItemText>{t("streamsort_user")}</StreamSortContentItemText>
+        </StreamSortContentItem>
+        <StreamSortContentItem>
+          <StreamSortContentItemText>{t("streamsort_artist")}</StreamSortContentItemText>
+        </StreamSortContentItem>
+      </StreamSortPopWrapper>
+    </>
+  );
 }
 
-export default StreamSort
+const StreamSortPopWrapper = styled.div`
+  /* width: 100px; */
+  height: auto;
+  background: #191922;
+  border-radius: 10px;
+  box-sizing: border-box;
+`;
+
+const StreamSortContentItem = styled.div`
+  /* width: 100%; */
+  height: 40px;
+  border-top: 1px solid rgba(139, 137, 153, 0.2);
+  color: #ffffff;
+  display: flex;
+  justify-content: center;
+  &::first-of-type {
+    border: none;
+    display: flex;
+    justify-content: center;
+  }
+`;
+
+const StreamSortContentItemText = styled.div`
+  /* width: 100%; */
+  height: 17px;
+  cursor: pointer;
+  padding: 12px 12px;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 17px;
+  /* identical to box height */
+`;
+
+export default StreamSort;
